814d9b60009a806e4ed9991c3214293d42eff720
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-06-11 22:12:42 -0400

    added logic for entry

--- berryface/urls.py
+++ berryface/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^clear_db/', views.clear_db, name="clear_db"),
     url(r'^insert_types/', views.insert_types, name="insert_types"),
     url(r'^add_sensor/', views.add_sensor, name="add_sensor"),
+    url(r'^add_entry/', views.add_entry, name="add_entry"),
 ]
